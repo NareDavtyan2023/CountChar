a2b0f545a7e3b886b9481bcc07f01466740f665c
Admin <Admin@DESKTOP-A06UMIO>
2023-06-12 20:54:55 +0400

    Add project files.

---
+++ CountChar/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CountChar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string strFisrt;
+            char charcount;
+            int Count = 0;
+            Console.Write(  "Input text: ");
+            strFisrt= Console.ReadLine();
+            Console.WriteLine(  "Count:");
+            charcount = Convert.ToChar(strFisrt);
+            Console.WriteLine("Count is:");
+            foreach(char chr in strFisrt)
+            {
+                if(chr==charcount)
+                {
+                    Count++;
+                }
+            }
+            Console.Write(Count);
+        }
+       
+    }
+}
